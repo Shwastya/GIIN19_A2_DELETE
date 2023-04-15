be3891ab17d6225fe04c648cfe5482946489448d
Jose L Rosa <jose.l.rosa.m@gmail.com>
2023-04-15 23:04:05 +0200

    java app

---
+++ ConsProdBad.java
@@ -0,0 +1,53 @@
+// Implementacion incorrecta del problema productor/consumidor
+class Q {
+   int n;
+   
+   synchronized int get () {
+	System.out.println("Consume: "+n);
+	return n;
+   }
+
+   synchronized void put (int n) {
+	this.n=n;
+	System.out.println("Produce: "+n);
+   }
+}
+
+class Producer implements Runnable {
+   Q q;
+
+   Producer(Q q) {
+	this.q=q;
+	new Thread(this,"Productor").start();
+   }
+
+   public void run() {
+       int i=0;
+       while(true) q.put(i++);
+   }
+}
+
+class Consumer implements Runnable {
+   Q q;
+
+   Consumer(Q q) {
+	this.q=q;
+	new Thread(this,"Consumidor").start();
+   }
+
+   public void run() {
+       while(true) q.get();
+
+   }
+}
+
+class PC {
+   public static void main(String args[]) {
+       Q q = new Q();
+      new Producer(q);
+      new Consumer(q);
+
+      System.out.println("Pulse Ctrl-C para finalizar");
+   }    
+}
+
